905fe18c9c380e173f464ae5c3235c5ca568af7b
gsantosdev <santosgustavo431@gmail.com>
2021-10-19 23:05:30 -0300

    Alterando nome titulo venda de serviço

--- src/views/comercial/comercial-home.js
+++ src/views/comercial/comercial-home.js
@@ -9,13 +9,16 @@ import Venda from "./venda";
 
 class ComercialHome extends React.Component {
 
+  constructor(props) {
+    super(props)
+  }
   render() {
     return (
       <div className="container-fluid">
 
         <Tabs unmountOnExit defaultActiveKey="efetuar_venda" id="uncontrolled-tab">
-          <Tab eventKey="efetuar_venda" title="Venda">
-            <Card title="Efetuar Venda">
+          <Tab eventKey="efetuar_venda" title="Venda de serviço">
+            <Card title="Efetuar Venda de Serviço">
               <Venda />
             </Card>
           </Tab>
