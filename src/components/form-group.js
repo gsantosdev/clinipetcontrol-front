7c9080eab51257957a4d5e27e9c41f814467c179
gsantosdev <santosgustavo431@gmail.com>
2021-10-28 22:37:57 -0300

    add hidden prop no formgroup

--- src/components/form-group.js
+++ src/components/form-group.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function FormGroup(props) {
     return (
-        <div className="form-group" style={{  margin: '0.2rem', minWidth: 'fit-content' }}>
+        <div hidden={props.hidden} className="form-group" style={{  margin: '0.2rem', minWidth: 'fit-content' }}>
             <label style={{fontSize:props.fontSize}} htmlFor={props.htmlFor}>{props.label}</label>
             {props.children}
 
